9f4cd60e8aa4d93a616465ab9223a32b530fb98f
jsparedes <js.paredes.merino@gmail.com>
2022-12-23 07:52:20 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import { eval_number, saludo } from "./mimodulo.js";
+
+saludo()
+eval_number(7)
+eval_number(6)
+eval_number(30)
---
+++ mimodulo.js
@@ -0,0 +1,12 @@
+export function eval_number(num) {
+    if (num % 2) {
+        console.log(`${num} es un número impar`);
+    }
+    else {
+        console.log(`${num} es un número par`);
+    }
+}
+
+export function saludo() {
+    console.log("Hola 👋")
+}
